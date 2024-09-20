a1b3ee64dab5d4e746935574dc86d70baa70bc8b
lennartkapanke <lkapanke@dev5310.com>
2024-09-20 03:49:46 +0200

    omg endlich was ein dreck

--- mobile_parking/lib/page2.dart
+++ mobile_parking/lib/page2.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_parking/model/bookings.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import '../service/api_service.dart';
+import 'ui/booking_dialog.dart';
 
 class Page2 extends StatefulWidget {
   const Page2({super.key});
@@ -69,108 +71,12 @@ class _Page2State extends State<Page2> {
 
   // Dialog für das automatische Buchen eines Parkplatzes
   void _showBookingDialog(DateTime selectedDate) {
-    TimeOfDay? _startTime;
-    TimeOfDay? _endTime;
-    bool _isAllDay = true;
-
-    Future<void> _bookParkingSlot() async {
-      final String bookingDate = DateFormat('yyyy-MM-dd').format(selectedDate);
-      try {
-        await apiService.autoBookParkingLot(
-          bookingDate: bookingDate,
-          startTime: !_isAllDay && _startTime != null ? _startTime!.format(context) : null,
-          endTime: !_isAllDay && _endTime != null ? _endTime!.format(context) : null,
-        );
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Parkplatz erfolgreich gebucht!')),
-        );
-        Navigator.of(context).pop();
-        fetchBookings(); // Aktualisiere Buchungen
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Fehler bei der Buchung')),
-        );
-      }
-    }
-
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Parkplatz buchen'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Checkbox für ganztägig buchen
-              Row(
-                children: [
-                  Checkbox(
-                    value: _isAllDay,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _isAllDay = value ?? true;
-                      });
-                    },
-                  ),
-                  const Text('Ganztägig buchen'),
-                ],
-              ),
-              if (!_isAllDay)
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        final TimeOfDay? picked = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-                        if (picked != null) {
-                          setState(() {
-                            _startTime = picked;
-                          });
-                        }
-                      },
-                      child: Text(
-                        _startTime != null
-                            ? 'Startzeit: ${_startTime!.format(context)}'
-                            : 'Startzeit auswählen',
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final TimeOfDay? picked = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                        );
-                        if (picked != null) {
-                          setState(() {
-                            _endTime = picked;
-                          });
-                        }
-                      },
-                      child: Text(
-                        _endTime != null
-                            ? 'Endzeit: ${_endTime!.format(context)}'
-                            : 'Endzeit auswählen',
-                      ),
-                    ),
-                  ],
-                ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Abbrechen'),
-            ),
-            ElevatedButton(
-              onPressed: _bookParkingSlot,
-              child: const Text('Buchen'),
-            ),
-          ],
-        );
+        return BookingDialog(
+            selectedDate: selectedDate,
+            onBookingSuccess: fetchBookings);
       },
     );
   }
@@ -198,7 +104,9 @@ class _Page2State extends State<Page2> {
                 });
 
                 // Setze die Uhrzeit für den Vergleich auf 00:00:00
-                DateTime selectedDayWithoutTime = selectedDay.copyWith(hour: 0, minute: 0, second: 0, millisecond: 0);
+                DateTime selectedDayWithoutTime =
+                DateTime(selectedDay.year, selectedDay.month, selectedDay.day)
+                    .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0);
 
                 if (_bookedDays.containsKey(selectedDayWithoutTime)) {
                   _showBookingDetails(_bookedDays[selectedDayWithoutTime]!);
